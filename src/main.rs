913a68619ca15fac9ef89cb3b23fd3d1b5970054
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-03-22 18:27:45 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// 1. emulate
+// nrfutil pkg generate --application loopback.hex --hw-version 52 --sd-req 0x00 --application-version 0 loopbacktest
+
+fn main() {
+    println!("Hello, world!");
+}

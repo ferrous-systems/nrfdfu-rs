ba83b0df1de5bd8eaf1b13dd508d024f968e4263
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-03-25 17:00:22 +0100

    send protocol version request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
-// 1. emulate
-// nrfutil pkg generate --application loopback.hex --hw-version 52 --sd-req 0x00 --application-version 0 loopbacktest
+use std::{path::Path, time::Duration};
+use serialport::available_ports;
+use std::error::Error;
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+const USB_VID: u16 = 0x1915;
+const USB_PID: u16 = 0x521f;
 
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Ok(()) => {}
+        Err(e) => {
+            eprintln!("error: {}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run() -> Result<()> {
+    let matching_ports: Vec<_> = available_ports()?.into_iter().filter(|port| {
+        match &port.port_type {
+            serialport::SerialPortType::UsbPort(usb) => {
+                usb.vid == USB_VID && usb.pid == USB_PID
+            }
+            _ => false,
+        }
+    }).collect();
+
+    let mut port = match matching_ports.len() {
+        0 => return Err(format!("no matching USB serial device found").into()),
+        1 => serialport::new(&matching_ports[0].port_name, 115200)
+                            .timeout(Duration::from_millis(1000))
+                            .open()?,
+        _ => return Err(format!("multiple matching USB serial devices found").into()),
+    };
+
+    let mut slip_enc = slip_codec::Encoder::new();
+    let mut slip_dec = slip_codec::Decoder::new();
+
+    slip_enc.encode(&[0x00], &mut port)?;
+
+    let mut response = vec![];
+    slip_dec.decode(&mut port, &mut response).map_err(|e| format!("{:?}", e))?;
+
+    println!("{:?}", response);
+
+    Ok(())
 }

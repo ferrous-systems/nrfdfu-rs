551b8610c3d1888b12c2d86930090f2cec90cb42
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2021-03-26 17:07:19 +0100

    Refactor request/response mechanism, complete error handling
    Co-authored-by: Lotte Steenbrink <Lotterleben@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::{time::Duration};
-use serialport::available_ports;
+use num_traits::FromPrimitive;
+use serialport::{available_ports, ClearBuffer, SerialPort};
 use std::error::Error;
+use std::time::Duration;
 
 mod messages;
 
-use messages::{NrfDfuOpCode::ProtocolVersion, NrfDfuResponse};
+use messages::{
+    DfuError, ExtError, HardwareVersionRequest, HardwareVersionResponse, NrfDfuResultCode,
+    ProtocolVersionRequest, Request, Response, NRF_DFU_PREAMBLE,
+};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 const USB_VID: u16 = 0x1915;
 const USB_PID: u16 = 0x521f;
-
 
 fn main() {
     match run() {
@@ -23,33 +26,148 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    let matching_ports: Vec<_> = available_ports()?.into_iter().filter(|port| {
-        match &port.port_type {
-            serialport::SerialPortType::UsbPort(usb) => {
-                usb.vid == USB_VID && usb.pid == USB_PID
-            }
+    let matching_ports: Vec<_> = available_ports()?
+        .into_iter()
+        .filter(|port| match &port.port_type {
+            serialport::SerialPortType::UsbPort(usb) => usb.vid == USB_VID && usb.pid == USB_PID,
             _ => false,
-        }
-    }).collect();
+        })
+        .collect();
 
-    let mut port = match matching_ports.len() {
+    let port = match matching_ports.len() {
         0 => return Err(format!("no matching USB serial device found").into()),
         1 => serialport::new(&matching_ports[0].port_name, 115200)
-                            .timeout(Duration::from_millis(1000))
-                            .open()?,
+            .timeout(Duration::from_millis(1000))
+            .open()?,
         _ => return Err(format!("multiple matching USB serial devices found").into()),
     };
 
-    let mut slip_enc = slip_codec::Encoder::new();
-    let mut slip_dec = slip_codec::Decoder::new();
-
-    slip_enc.encode(&[ProtocolVersion as u8], &mut port)?;
-
-    let mut response_bytes = vec![];
-    slip_dec.decode(&mut port, &mut response_bytes).map_err(|e| format!("{:?}", e))?;
-
-    let response = NrfDfuResponse::from_bytes(&response_bytes);
-    println!("{:?}", response);
+    let mut conn = BootloaderConnection::new(port)?;
+    let version = conn.fetch_protocol_version()?;
+    println!("protocol version: {}", version);
+    if false {
+        // TODO: this doesn't work
+        let hw_version = conn.fetch_hardware_version()?;
+        println!("hardware version: {:?}", hw_version);
+    }
 
     Ok(())
+}
+
+struct BootloaderConnection {
+    slip_enc: slip_codec::Encoder,
+    slip_dec: slip_codec::Decoder,
+    serial: Box<dyn SerialPort>,
+}
+
+impl BootloaderConnection {
+    fn new(serial: Box<dyn SerialPort>) -> Result<Self> {
+        serial.clear(ClearBuffer::All)?;
+        Ok(Self {
+            slip_enc: slip_codec::Encoder::new(),
+            slip_dec: slip_codec::Decoder::new(),
+            serial,
+        })
+    }
+
+    fn request<R: Request>(&mut self, req: R) -> Result<R::Response> {
+        let mut buf = vec![R::OPCODE as u8];
+        req.write_payload(&mut buf)?;
+        eprintln!("req: {:x?}", buf);
+        self.slip_enc.encode(&buf, &mut self.serial)?;
+
+        let mut response_bytes = vec![];
+        self.slip_dec
+            .decode(&mut self.serial, &mut response_bytes)
+            .map_err(|e| format!("{:?}", e))?;
+        eprintln!("resp: {:x?}", response_bytes);
+
+        // Response format:
+        // - Fixed byte 0x60
+        // - Request opcode
+        // - Response result code
+        // - Response payload
+        if response_bytes.len() < 3 {
+            return Err(format!(
+                "truncated response (expected at least 3 bytes, got {})",
+                response_bytes.len()
+            )
+            .into());
+        }
+
+        if response_bytes[0] != NRF_DFU_PREAMBLE {
+            return Err(format!(
+                "malformed response (expected nrf DFU preamble 0x60, got 0x{:02x})",
+                response_bytes[0]
+            )
+            .into());
+        }
+
+        if response_bytes[1] != R::OPCODE as u8 {
+            return Err(format!(
+                "malformed response (expected echoed opcode {:?} (0x{:02x}), got 0x{:02x})",
+                R::OPCODE,
+                R::OPCODE as u8,
+                response_bytes[1]
+            )
+            .into());
+        }
+
+        let result: NrfDfuResultCode =
+            NrfDfuResultCode::from_u8(response_bytes[2]).ok_or_else(|| {
+                format!(
+                    "malformed response (invalid result code 0x{:02x})",
+                    response_bytes[2]
+                )
+            })?;
+
+        match result {
+            NrfDfuResultCode::Success => {}
+            NrfDfuResultCode::ExtError => match response_bytes.get(3) {
+                Some(byte) => {
+                    let ext_error: ExtError = ExtError::from_u8(*byte).ok_or_else(|| {
+                        format!(
+                            "malformed response (unknown extended error code 0x{:02x})",
+                            byte
+                        )
+                    })?;
+
+                    return Err(DfuError {
+                        code: NrfDfuResultCode::ExtError,
+                        ext_error: Some(ext_error),
+                    }
+                    .into());
+                }
+                None => {
+                    return Err(format!("malformed response (missing extended error byte)").into());
+                }
+            },
+            code => {
+                return Err(DfuError {
+                    code,
+                    ext_error: None,
+                }
+                .into())
+            }
+        }
+
+        let mut response_bytes = &response_bytes[3..];
+        let response = R::Response::read_payload(&mut response_bytes)?;
+
+        if !response_bytes.is_empty() {
+            return Err(format!("trailing bytes in response").into());
+        }
+
+        Ok(response)
+    }
+
+    fn fetch_protocol_version(&mut self) -> Result<u8> {
+        let version_response = self.request(ProtocolVersionRequest)?;
+        Ok(version_response.version)
+    }
+
+    fn fetch_hardware_version(&mut self) -> Result<HardwareVersionResponse> {
+        let hw_version_response = self.request(HardwareVersionRequest)?;
+        Ok(hw_version_response)
+    }
 }
--- src/messages.rs
+++ src/messages.rs
@@ -1,6 +1,11 @@
+use std::{
+    error::Error,
+    fmt,
+    io::{self, Read, Write},
+};
 
+use byteorder::{ReadBytesExt, LE};
 use num_derive::FromPrimitive;
-use num_traits::FromPrimitive;
 
 pub const NRF_DFU_PREAMBLE: u8 = 0x60;
 
@@ -9,60 +14,126 @@ pub const NRF_DFU_PREAMBLE: u8 = 0x60;
 #[derive(FromPrimitive, Debug)]
 pub enum NrfDfuOpCode {
     ProtocolVersion = 0x0,
+    HardwareVersionGet = 0x0A,
 }
 #[derive(FromPrimitive, Debug)]
 pub enum NrfDfuResultCode {
-    Invalid                 = 0x00,    // Invalid opcode.
-    Success                 = 0x01,    // Operation successful.
-    OpCodeNotSupported      = 0x02,    // Opcode not supported.
-    InvalidParameter        = 0x03,    // Missing or invalid parameter value.
-    InsufficientResources    = 0x04,    // Not enough memory for the data object.
-    InvalidObject           = 0x05,    // Data object does not match the firmware and hardware requirements, the signature is wrong, or parsing the command failed.
-    UnsupoortedType         = 0x07,    // Not a valid object type for a Create request.
-    OperationNotPermitted   = 0x08,    // The state of the DFU process does not allow this operation.
-    OperationFailed         = 0x0A,    // Operation failed.
-    ExtError                = 0x0B,    // Extended error. The next byte of the response contains the error code of the extended error (see @ref nrf_dfu_ext_error_code_t.
+    Invalid = 0x00,               // Invalid opcode.
+    Success = 0x01,               // Operation successful.
+    OpCodeNotSupported = 0x02,    // Opcode not supported.
+    InvalidParameter = 0x03,      // Missing or invalid parameter value.
+    InsufficientResources = 0x04, // Not enough memory for the data object.
+    InvalidObject = 0x05, // Data object does not match the firmware and hardware requirements,
+    // the signature is wrong, or parsing the command failed.
+    UnsupoortedType = 0x07, // Not a valid object type for a Create request.
+    OperationNotPermitted = 0x08, // The state of the DFU process does not allow this operation.
+    OperationFailed = 0x0A, // Operation failed.
+    ExtError = 0x0B, // Extended error. The next byte of the response contains the error code of
+                     // the extended error (see @ref nrf_dfu_ext_error_code_t.
 }
 
 #[derive(Debug)]
-pub enum NrfDfuResponseDetails {
-    ProtocolVersion(u8),               // Protocol version response
+pub struct DfuError {
+    pub code: NrfDfuResultCode,
+    pub ext_error: Option<ExtError>,
 }
 
-#[derive(Debug)]
-pub struct NrfDfuResponse {
-    /// should always be NRF_DFU_PREAMBLE
-    preamble: u8,
-    /// the opcode of the request we're responding to
-    request: NrfDfuOpCode,
-    /// Result of the operation. (e.g. error code)
-    result: NrfDfuResultCode,
-    // response (e.g. the thing you asked for)
-    response_details: NrfDfuResponseDetails
+#[derive(FromPrimitive, Debug)]
+pub enum ExtError {
+    NoError = 0x00,
+    InvalidErrorCode = 0x01,
+    WrongCommandFormat = 0x02,
+    UnknownCommand = 0x03,
+    InitCommandInvalid = 0x04,
+    FwVersionFailure = 0x05,
+    HwVersionFailure = 0x06,
+    SdVersionFailure = 0x07,
+    SignatureMissing = 0x08,
+    WrongHashType = 0x09,
+    HashFailed = 0x0A,
+    WrongSignatureType = 0x0B,
+    VerificationFailed = 0x0C,
+    InsufficientSpace = 0x0D,
 }
 
-impl NrfDfuResponse {
-    pub fn from_bytes(bytes: &Vec<u8>) -> Self {
-        // All responses start with `0x60`
-        let preamble = bytes[0];
-        assert_eq!(preamble, NRF_DFU_PREAMBLE);
-
-        let request = FromPrimitive::from_u8(bytes[1]).unwrap();
-
-        // TODO split off into separate handler functions depending on `request` once this get more complex
-        let (result, response_details) = match request {
-            NrfDfuOpCode::ProtocolVersion => {
-                let result = FromPrimitive::from_u8(bytes[2]).unwrap();
-                let protocol_version = bytes[3];
-                (result, NrfDfuResponseDetails::ProtocolVersion(protocol_version))
-            }
-        };
-
-        NrfDfuResponse {
-            preamble,
-            request,
-            result,
-            response_details,
+impl fmt::Display for DfuError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // TODO add error code names + descriptions
+        write!(f, "DFU error code: {:?}", self.code)?;
+        if self.ext_error.is_some() {
+            write!(f, "| extended error code: {:?}", self.ext_error)?;
         }
+        Ok(())
+    }
+}
+
+impl Error for DfuError {}
+
+pub trait Request {
+    const OPCODE: NrfDfuOpCode;
+    type Response: Response;
+
+    fn write_payload<W: Write>(&self, writer: W) -> io::Result<()>;
+}
+
+pub trait Response: Sized {
+    fn read_payload<R: Read>(reader: R) -> io::Result<Self>;
+}
+
+pub struct ProtocolVersionRequest;
+
+impl Request for ProtocolVersionRequest {
+    const OPCODE: NrfDfuOpCode = NrfDfuOpCode::ProtocolVersion;
+
+    type Response = ProtocolVersionResponse;
+
+    fn write_payload<W: Write>(&self, _writer: W) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub struct ProtocolVersionResponse {
+    pub version: u8,
+}
+
+impl Response for ProtocolVersionResponse {
+    fn read_payload<R: Read>(mut reader: R) -> io::Result<Self> {
+        Ok(Self {
+            version: reader.read_u8()?,
+        })
+    }
+}
+
+pub struct HardwareVersionRequest;
+
+impl Request for HardwareVersionRequest {
+    const OPCODE: NrfDfuOpCode = NrfDfuOpCode::HardwareVersionGet;
+
+    type Response = HardwareVersionResponse;
+
+    fn write_payload<W: Write>(&self, _writer: W) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct HardwareVersionResponse {
+    // See FICR register docs
+    part: u32,
+    variant: u32,
+    rom_size: u32,
+    ram_size: u32,
+    rom_page_size: u32,
+}
+
+impl Response for HardwareVersionResponse {
+    fn read_payload<R: Read>(mut response_bytes: R) -> io::Result<Self> {
+        Ok(Self {
+            part: response_bytes.read_u32::<LE>()?,
+            variant: response_bytes.read_u32::<LE>()?,
+            rom_size: response_bytes.read_u32::<LE>()?,
+            ram_size: response_bytes.read_u32::<LE>()?,
+            rom_page_size: response_bytes.read_u32::<LE>()?,
+        })
     }
 }
